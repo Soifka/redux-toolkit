2e3be9be0b2220cf0043529a5c4bcdad4f7641c5
Julia Alekseeva <soifka@gmail.com>
2023-03-20 20:23:39 +0200

    Connect counter to redux state

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import  { connect } from 'react-redux';
+import { increment, decrement } from '../../store/slices/counterSlice';
 
-const Counter = () => {
+const Counter = (props) => {
+console.log(props)
+const { count, step, dispatch } = props;
+
     return (
         <div>
-            <p>Count: {1}</p>
-            <p>Step: {1}</p>
-            <button>Decrement</button>
-            <button>Increment</button>
+            <p>Count: {count}</p>
+            <p>Step: {step}</p>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <button onClick={() => dispatch(increment())}>Increment</button>
         </div>
     );
 }
 
-export default Counter;
+function mapStateToProps(state) {
+    return state;
+}
+ /* подписка на часть стейта -->
+function mapStateToProps(state) {
+    return {
+        count: state.count,
+        step: state.step
+    };
+}
+*/
+
+// компонент высшего порядка со стейтом -->
+// const withState = connect(mapStateToProps);
+
+// const CounterWithState = withState(Counter);
+
+//export default CounterWithState;
+
+export default connect(mapStateToProps)(Counter);
