5d3aa1385e89b004dfcdbc73bc71c55255254e10
Julia Alekseeva <soifka@gmail.com>
2023-03-21 20:59:46 +0200

    Refactor Counter (useSelector + useDispatch

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  { connect } from 'react-redux';
+import  { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement, setStep } from '../../store/slices/counterSlice';
 import { setLang } from '../../store/slices/langSlice';
 import CONSTANTS from '../../constants';
@@ -39,74 +39,38 @@ const translations = new Map([
 ])
 
 const Counter = (props) => {
-console.log(props)
-const { count, step, language, theme, increment, decrement, setStep, setLang } = props;
+    const language = useSelector((state) => state.lang);
+    const theme = useSelector((state) => state.theme);
+    const { count, step } = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
 
-const translation = translations.get(language);
-const { countText, stepText, incrementText, decrementText } = translation;
+    const setLanguage = (newLang) => dispatch(setLang(newLang));
+    const setNewStep = (newStep) => dispatch(setStep(newStep))
 
-const className = cx({
-    [styles.darkTheme]: theme === THEME.DARK,
-    [styles.lightTheme]: theme === THEME.LIGHT
-})
+    const translation = translations.get(language);
+    const { countText, stepText, incrementText, decrementText } = translation;
+
+    const className = cx({
+        [styles.darkTheme]: theme === THEME.DARK,
+        [styles.lightTheme]: theme === THEME.LIGHT
+    })
 
     return (
         <div className={className}>
-            <select value={language} onChange={({target: { value }}) => setLang(value)}>
+            <select value={language} onChange={({target: { value }}) => setLanguage(value)}>
                 {Object.values(LANGUAGE).map((langObj) => 
                     <option key={langObj.VALUE} value={langObj.VALUE}>{langObj.OPTION_TEXT}</option>
                 )}
             </select>
             <p>{countText}: {count}</p>
             <label>
-                {stepText}: <input type="number" value={step} onChange={({target: {value}}) => setStep(value)} />
+                {stepText}: <input type="number" value={step} onChange={({target: {value}}) => setNewStep(value)} />
             </label>
-            <button onClick={() => decrement()}>{decrementText}</button>
-            <button onClick={() => increment()}>{incrementText}</button>
+            <button onClick={() => dispatch(decrement())}>{decrementText}</button>
+            <button onClick={() => dispatch(increment())}>{incrementText}</button>
         </div>
     );
 }
 
-/* подписка на весь стейт -->
-function mapStateToProps(state) {
-    return state;
-}
-*/
 
- // подписка на часть стейта -->
-function mapStateToProps(state) {
-    return {
-        ...state.counter,
-        language: state.lang,
-        theme: state.theme
-    };
-}
-
-
-// компонент высшего порядка со стейтом -->
-// const withState = connect(mapStateToProps);
-
-// const CounterWithState = withState(Counter);
-
-//export default CounterWithState;
-
-/* !!!!!
-mapDispatchToProps --> функциональный вид
-function  mapDispatchToProps(dispatch) {
-    return {
-        incrementCb: () => dispatch(increment()),
-        decrementCb: () => dispatch(decrement()),
-        setStepCb: ({target: {value}}) => dispatch(setStep(value))
-    }
-}
-*/
-
-const mapDispatchToProps = {
-    increment,
-    decrement,
-    setStep,
-    setLang
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
