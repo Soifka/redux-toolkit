024a8218fada07c70f9b72c421fc8213e9a89cce
Julia Alekseeva <soifka@gmail.com>
2023-03-21 21:13:12 +0200

    Refactor actionCreators

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import  { useSelector, useDispatch } from 'react-redux';
 import { bindActionCreators } from '@reduxjs/toolkit';
-import { increment, decrement, setStep } from '../../store/slices/counterSlice';
-import { setLang } from '../../store/slices/langSlice';
+import * as counterActionCreators from '../../store/slices/counterSlice';
+import * as langActionCreators from '../../store/slices/langSlice';
 import CONSTANTS from '../../constants';
 import cx from 'classnames';
 import styles from './Counter.module.scss';
@@ -45,8 +45,8 @@ const Counter = (props) => {
     const { count, step } = useSelector((state) => state.counter);
     const dispatch = useDispatch();
 
-    const actionCreators = bindActionCreators(
-        {setLang, setStep, increment, decrement}, 
+    const { setLang, setStep, increment, decrement } = bindActionCreators(
+        {...counterActionCreators, ...langActionCreators}, 
         dispatch);
 
     const translation = translations.get(language);
@@ -59,17 +59,17 @@ const Counter = (props) => {
 
     return (
         <div className={className}>
-            <select value={language} onChange={({target: { value }}) => actionCreators.setLang(value)}>
+            <select value={language} onChange={({target: { value }}) => setLang(value)}>
                 {Object.values(LANGUAGE).map((langObj) => 
                     <option key={langObj.VALUE} value={langObj.VALUE}>{langObj.OPTION_TEXT}</option>
                 )}
             </select>
             <p>{countText}: {count}</p>
             <label>
-                {stepText}: <input type="number" value={step} onChange={({target: {value}}) => actionCreators.setStep(value)} />
+                {stepText}: <input type="number" value={step} onChange={({target: {value}}) => setStep(value)} />
             </label>
-            <button onClick={() => actionCreators.decrement()}>{decrementText}</button>
-            <button onClick={() => actionCreators.increment()}>{incrementText}</button>
+            <button onClick={() => decrement()}>{decrementText}</button>
+            <button onClick={() => increment()}>{incrementText}</button>
         </div>
     );
 }
