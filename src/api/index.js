b86ac88ae3a1251bd2827d62c4be2e07ab59834b
Julia Alekseeva <soifka@gmail.com>
2023-03-22 20:12:56 +0200

    Rewrite API request to axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,13 @@
+/*
 export async function getUsers() {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const users = await response.json();
+    return users;
+}
+*/
+import axios from "axios"
+
+export async function getUsers() {
+    const { data: users } = await axios.get('https://jsonplaceholder.typicode.com/users')
     return users;
 }
